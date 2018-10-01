2dd6b347cc4ad16e74ce07ec0d2b976de821c2d6
Pegu <hemanti.pegu@capgemini.com>
2018-10-01 12:12:02 +0530

    Comitted

---
+++ Employee.java
@@ -0,0 +1,9 @@
+class Employee{
+ private int empId=100;
+ public void getEmpId(){
+  System.out.println("The employeeid is: "+ this.empId);
+  }
+  public void setEmpId(int Id){
+  this.empId=Id;
+  }
+}
--- p1.java
+++ p1.java
@@ -1,5 +1,8 @@
-class p1{  
+public class p1{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java"); 
+		 employee emp=new employee();
+		 emp.setEmpId(15);
+		 emp.getEmpId();
     }  
 }  
